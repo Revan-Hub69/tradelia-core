51c3b760d10c75ad9976a20617a4e9403b3b8f06
Revan-Hub69 <topofferte@protonmail.ch>
2026-01-07 20:51:50 +0100

    Fix FaqSchema TypeScript error - Cast questions to array

--- hooks/useTranslations.ts
+++ hooks/useTranslations.ts
@@ -201,7 +201,7 @@ export function useTranslations() {
     },
 
     faq: {
-      questions: t('faq.questions')
+      questions: t('faq.questions') as any[]
     }
   }), [locale, t, setLocale]);
 
