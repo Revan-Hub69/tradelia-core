f99979f9e999ef7270bb712dbc6f9ee33b5eb4dc
Revan-Hub69 <topofferte@protonmail.ch>
2026-01-25 01:13:49 +0100

    fix: correct IntlMessages type merging for namespaces

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,17 @@
 // Use type safe message keys with `next-intl`
 type Messages = typeof import('../locales/en.json');
 
+// Namespace messages (modular translation system)
+type DashboardMessages = typeof import('../../messages/en/dashboard.json');
+type CommonMessages = typeof import('../../messages/en/common.json');
+type DashboardSettingsMessages = typeof import('../../messages/en/dashboard-settings.json');
+
+// Merge all message types
+type AllMessages = Messages & {
+  Dashboard: DashboardMessages;
+  Common: CommonMessages;
+  'DashboardSettings': DashboardSettingsMessages;
+};
+
 // eslint-disable-next-line ts/consistent-type-definitions
-declare interface IntlMessages extends Messages {}
+declare interface IntlMessages extends AllMessages {}
