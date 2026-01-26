5575d54f85fa44a2ee6a1c9f747bf7cd8a534426
Revan-Hub69 <topofferte@protonmail.ch>
2026-01-26 18:31:42 +0100

    fix: register new translation namespaces (Challenges, MyChallenges, Signals) in TypeScript types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,12 +5,18 @@ type Messages = typeof import('../locales/en.json');
 type CommonMessages = typeof import('../../messages/en/common.json');
 type DashboardSettingsMessages = typeof import('../../messages/en/dashboard-settings.json');
 type ContactMessages = typeof import('../../messages/en/contact.json');
+type ChallengesMessages = typeof import('../../messages/en/challenges.json');
+type MyChallengesMessages = typeof import('../../messages/en/my-challenges.json');
+type SignalsMessages = typeof import('../../messages/en/signals.json');
 
 // Merge all message types
 type AllMessages = Messages & {
   Common: CommonMessages;
   DashboardSettings: DashboardSettingsMessages;
   Contact: ContactMessages;
+  Challenges: ChallengesMessages;
+  MyChallenges: MyChallengesMessages;
+  Signals: SignalsMessages;
 };
 
 // eslint-disable-next-line ts/consistent-type-definitions
