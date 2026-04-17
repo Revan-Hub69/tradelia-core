f70ccb2fefae63170e73dc3e458cac36a02f24ac
Tradelia Dev <dev@tradelia.org>
2026-04-17 13:39:52 +0200

    fix: restore Env.ts in src/lib and update next.config.mjs import

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ import withNextIntl from 'next-intl/plugin';
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
-jiti('./src/libs/Env');
+jiti('./src/lib/Env');
 
 const withNextIntlConfig = withNextIntl();
 
