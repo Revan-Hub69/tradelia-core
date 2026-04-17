468bfe36f73c220b004358698fcf11210d64033d
Tradelia Dev <dev@tradelia.org>
2026-04-17 13:31:47 +0200

    chore: deep cleanup - remove legacy, supabase, storybook, migrations, unused configs

--- src/libs/i18nNavigation.ts
+++
@@ -1,8 +0,0 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-
-import { AllLocales, AppConfig } from '@/utils/AppConfig';
-
-export const { Link, usePathname, useRouter } = createSharedPathnamesNavigation({
-  locales: AllLocales,
-  localePrefix: AppConfig.localePrefix,
-});
